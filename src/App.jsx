2a4a0de326ce13358dd888d62be6632ceb66d263
angelespini <angelesbpini@gmail.com>
2022-08-02 14:11:56 -0300

    grillas react

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="general">
+
+      <button className='buttonUno'>1col</button>
+      <button className='buttonDos'>2col</button>
+      <button className='buttonTres'>3col</button>
+      <button className='buttonCuatro'>4col</button>
+      <button className='buttonCinco'>2col</button>
+      <button className='buttonSeis'>3col</button>
+      <button className='buttonSiete'>1col</button>
+      <button className='buttonOcho'>2col</button>
+
+    </div>
+    </>
+  );
+}
+
+export default App;
