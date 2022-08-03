6384ad9d65cf309f3bc2b7cbfc1063e167c20190
angelespini <angelesbpini@gmail.com>
2022-08-03 11:37:34 -0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,15 @@ function App() {
       <button className='buttonOcho'>2col</button>
 
     </div>
+    <p>EJEMPLO 2</p>
+    <div className="generalDos">
+      <h1>Mobile H1-32px</h1>
+      <p>Lorem impsum blabla bla 12431242 gjiuoñEFHJAOS RJ823UR9 mpsum blabla bla 124 m impsum blabla bla 12431242 gjiuoñEFHJAOS RJ823UR9 mpsum blabla bla 124</p>
+      <div className="verMas">
+        <p>ver mas</p> <span>+</span>
+      </div>
+      <button className='secondButton'>4col</button>
+    </div>
     </>
   );
 }
