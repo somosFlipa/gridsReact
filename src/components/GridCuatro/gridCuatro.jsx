ba0a2bc17be57ca0149dfa889e84c64cc16f9853
angelespini <angelesbpini@gmail.com>
2022-08-16 10:55:38 -0300

    nuevos grid

--- src/components/GridCuatro/gridCuatro.jsx
+++ src/components/GridCuatro/gridCuatro.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import './gridCuatro.css';
+
 
 const GridCuatro = ()=>{
     return(
         <>
+        <div className="body_gridCuatro">
+            <div className="body_gridCuatro__general">
+            <div className="prueba">este esun div de prueba</div>
+            <div className="prueba">este esun div de prueba</div>
+            <div className="prueba">este esun div de prueba</div>
+            <div className="prueba">este esun div de prueba</div>
+            </div>
+        </div>
+        
         </>
     )
 }
--- src/components/gridTres/gridTres.jsx
+++ src/components/gridTres/gridTres.jsx
@@ -11,7 +11,13 @@ const GridTres = ()=>{
         <div className="general__GridTres">
             <div className="nav">
                 <div className="logo"></div>
-                <div className="menu"></div>
+                <div className="menu">
+                    <ul>
+                        <li>HOME</li>
+                        <li>SHOP</li>
+                        <li>CONTACTO</li>
+                    </ul>
+                </div>
             </div>
             <div className="hero">
                 <h1>#SOMOSFLIPA</h1>
@@ -20,8 +26,16 @@ const GridTres = ()=>{
             <div className="bloques">
             <div className="bloqueUno bloque__GridTres"></div>
             <div className="bloqueDos bloque__GridTres"></div>
+            <div className="bloqueDos__Duplicado bloque__GridTres"></div>
             <div className="bloqueTres bloque__GridTres"></div>
-            <div className="bloqueCuatro bloque__GridTres"></div>
+            <div className="bloqueCuatro bloque__GridTres">
+                <label htmlFor="">Nombre</label>
+                <input type="text" />
+                <label htmlFor="">Mail</label>
+                <input type="text" />
+                <label htmlFor="">Mensaje</label>
+                <input type="text" />
+            </div>
             <button className="gridTres__Button buttonGrid">BOTON</button>                
             </div>
 
